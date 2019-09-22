f44f31e60bb03c72c93efb7da27d4a5ef155baf0
Waleed Gadelkareem <gadelkareem@gmail.com>
2019-09-22 03:02:33 +0200

    add new lessons

--- src/codility/main.go
+++ src/codility/main.go
@@ -16,7 +16,155 @@ func main() {
 	//fmt.Printf("%+v\n", PermCheck([]int{4, 1, 2, 3}))
 	//fmt.Printf("%+v\n", FrogRiverOne(5, []int{1, 3, 1, 4, 2, 3, 5, 4}))
 	//fmt.Printf("%+v\n", MaxCounters(5, []int{3, 4, 4, 6, 1, 4, 4}))
-	fmt.Printf("%+v\n", MissingInteger([]int{1, 3, 6, 4, 1, 2}))
+	//fmt.Printf("%+v\n", MissingInteger([]int{1, 3, 6, 4, 1, 2}))
+	//fmt.Printf("%+v\n", PassingCars([]int{0, 1, 0, 1, 1}))
+	//fmt.Printf("%+v\n", GenomicRangeQuery("CAGCCTA", []int{2, 5, 0}, []int{4, 5, 6}))
+	//fmt.Printf("%+v\n", MinAvgTwoSlice([]int{4, 2, 2, 5, 1, 5, 8}))
+	//fmt.Printf("%+v\n", MaxProductOfThree([]int{-3, 1, 2, -2, 5, 6}))
+	//fmt.Printf("%+v\n", Distinct([]int{2, 1, 1, 2, 3, 1}))
+	//fmt.Printf("%+v\n", Triangle([]int{50, 40, 20, 10, 1}))
+	fmt.Printf("%+v\n", NumberOfDiscIntersections([]int{1, 5, 2, 1, 4, 0}))
+}
+
+// https://app.codility.com/demo/results/trainingJZ8UZK-4ET/
+func NumberOfDiscIntersections(A []int) int {
+	res := 0
+	n := len(A)
+	r, l := make([]int, n), make([]int, n)
+	for i := range A {
+		l[i] = i - A[i]
+		r[i] = i + A[i]
+	}
+	sort.Ints(l)
+	sort.Ints(r)
+	for i := range A {
+		end := r[i]
+		count := sort.Search(len(l), func(i int) bool {
+			return l[i] > end
+		})
+		count -= i + 1
+		res += count
+		if res > 10000000 {
+			return -1
+		}
+	}
+	return res
+}
+
+// https://app.codility.com/demo/results/training6HR5FG-G2F/
+func Distinct(A []int) int {
+	h := make(map[int]bool)
+
+	for _, n := range A {
+		h[n] = true
+	}
+
+	return len(h)
+}
+
+// https://app.codility.com/demo/results/trainingFW7JRE-ZEX/
+func MaxProductOfThree(A []int) int {
+	N := len(A)
+	a, b, c := -1<<31, -1<<31, -1<<31
+	d, e := 1<<31-1, 1<<31-1
+
+	for i := 0; i < N; i++ {
+		if A[i] > a {
+			c = b
+			b = a
+			a = A[i]
+		} else if A[i] > b {
+			c = b
+			b = A[i]
+		} else if A[i] > c {
+			c = A[i]
+		}
+
+		if A[i] < d {
+			e = d
+			d = A[i]
+		} else if A[i] < e {
+			e = A[i]
+		}
+
+	}
+	return int(math.Max(float64(a)*float64(b)*float64(c), float64(a)*float64(d)*float64(e)))
+}
+
+// https://app.codility.com/demo/results/trainingBHJWDB-3YE/
+func MinAvgTwoSlice(A []int) int {
+	N := len(A)
+	minAvg := float64(1<<32 - 1)
+	r := 0
+	var avg float64
+	for i := 0; i < N-1; i++ {
+		if i+1 < N {
+			avg = (float64(A[i]) + float64(A[i+1])) / 2
+			if avg < minAvg {
+				minAvg = avg
+				r = i
+			}
+		}
+		if i+2 < N {
+			avg = (float64(A[i]) + float64(A[i+1]) + float64(A[i+2])) / 3
+			if avg < minAvg {
+				minAvg = avg
+				r = i
+			}
+		}
+	}
+	return r
+}
+
+// https://app.codility.com/demo/results/training6R6GDV-9CS/
+func GenomicRangeQuery(S string, P []int, Q []int) []int {
+	sLn := len(S) + 1
+	pSum := make(map[rune][]int, 3)
+	pSum['A'] = make([]int, sLn)
+	pSum['C'] = make([]int, sLn)
+	pSum['G'] = make([]int, sLn)
+	pSum['T'] = make([]int, sLn)
+	for i := 1; i < sLn; i++ {
+		for x := range pSum {
+			pSum[x][i] = pSum[x][i-1]
+		}
+		pSum[rune(S[i-1])][i]++
+	}
+	pLn := len(P)
+	r := make([]int, pLn)
+	for i := 0; i < pLn; i++ {
+		if pSum['A'][Q[i]+1]-pSum['A'][P[i]] > 0 {
+			r[i] = 1
+		} else if pSum['C'][Q[i]+1]-pSum['C'][P[i]] > 0 {
+			r[i] = 2
+		} else if pSum['G'][Q[i]+1]-pSum['G'][P[i]] > 0 {
+			r[i] = 3
+		} else {
+			r[i] = 4
+		}
+	}
+	return r
+}
+
+// https://app.codility.com/demo/results/trainingNDRTN3-Q5Q/
+func PassingCars(A []int) int {
+	N := len(A)
+	P := make([]int, N)
+
+	for i := N - 2; i > -1; i-- {
+		P[i] = P[i+1] + A[i+1]
+	}
+	pairs := 0
+	for i, v := range A {
+		if v == 0 {
+			pairs += P[i]
+			if pairs > 1000000000 {
+				return -1
+			}
+		}
+	}
+
+	return pairs
 }
 
 // https://app.codility.com/demo/results/trainingEARXAJ-4P7/
@@ -213,4 +361,9 @@ func sum(A []int, x, y, z int) int {
 		v += A[i]
 	}
 	return v
+}
+
+func minInSlice(v []int) int {
+	sort.Ints(v)
+	return v[0]
 }
